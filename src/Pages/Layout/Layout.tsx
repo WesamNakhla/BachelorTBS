08248de7bb4aee32f5d321ccf124e08b58d6aec9
bello hadi <bellohadi82@gmail.com>
2025-03-14 17:50:13 +0100

    :bug: fixed alignment issue

--- src/Pages/Layout/Layout.tsx
+++ src/Pages/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import Footer from "../../Components/Footer/Footer";
 const Layout = ({children}: any)=>{
     return(
         <>
-           <div className="w-[100%] h-[100%]">
+           <div className="w-[100%] h-[100vh]">
                 <Header />
                 {children}
                 <Footer />
