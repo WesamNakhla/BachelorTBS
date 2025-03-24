c0a40a8a6ca9ce465be5c5a7bc6e7f59a057aa21
bello hadi <bellohadi82@gmail.com>
2025-03-24 07:57:37 +0100

    :bug: fixed chakra issue

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
    
   </StrictMode>,
