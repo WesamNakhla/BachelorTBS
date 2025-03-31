69327a410eb4cf7a3a5e810a0f5bb4536a0147da
Atridox <siyabendgcc@hotmail.com>
2025-03-31 22:36:14 +0100

    :sparkles: added all icons to the sidebar

--- src/Components/DashboardSidebar/DashboardSidebar.tsx
+++ src/Components/DashboardSidebar/DashboardSidebar.tsx
@@ -3,19 +3,26 @@ import { RxDashboard } from "react-icons/rx";
 import { IoSettingsOutline } from "react-icons/io5";
 import { CiLight } from "react-icons/ci";
 import { MdOutlineDarkMode } from "react-icons/md";
+import { IoShieldCheckmarkSharp } from "react-icons/io5";
+import { FaUser } from "react-icons/fa";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { FiFileText } from "react-icons/fi";
+import { FaLock } from "react-icons/fa";
+import { FaFileSignature } from "react-icons/fa";
+
 
 const navigation = [
-    { "to": "/Dashboard", "text": "Dashboard"  },
-    { "to": "/Faktura", "text": "Fakturaer"  },
-    { "to": "/Kunder", "text": " Kunder"  },
-    { "to": "/Profiler", "text": " Profiler"  },
-    { "to": "/Settings", "text": "Settings"  },
-    { "to": "/User", "text": "User"  },
-    { "to": "/role", "text": "Role"  },
-    { "to": "/Security", "text": "Security Settings"  },
-    { "to": "/Logs", "text": "Activity Log"  },
-    { "to": "/Reports", "text": "Reports"  },
-    { "to": "/Notifications", "text": "Notifications"  },
+    { "to": "/Dashboard", "text": "Dashboard", "icon": <RxDashboard /> },
+    { "to": "/Faktura", "text": "Fakturaer" , "icon": <FiFileText /> },
+    { "to": "/Kunder", "text": " Kunder", "icon": <FaUser />  },
+    { "to": "/Profiler", "text": " Profiler", "icon": <FaUser />  },
+    { "to": "/Settings", "text": "Settings", "icon": <IoSettingsOutline />},
+    { "to": "/User", "text": "User", "icon": <FaUser />  },
+    { "to": "/role", "text": "Role", "icon": <IoShieldCheckmarkSharp />  },
+    { "to": "/Security", "text": "Security Settings", "icon": <FaLock />  },
+    { "to": "/Logs", "text": "Activity Log", "icon": <FaFileSignature />  },
+    { "to": "/Reports", "text": "Reports", "icon": <FiFileText />   },
+    { "to": "/Notifications", "text": "Notifications", "icon": <IoMdNotificationsOutline />  },
     
     
 
@@ -25,52 +32,20 @@ const DashboardSidebar = ()=>{
         <>
             <div className="flex flex-col items-center w-[25%] h-full bg-[#A2A1A8]/5 p-5">
                 <h1 className="font-lexend font-bold text-[#31A2F2]">TBS</h1>
-                <div className="flex flex-col justify-between h-[600px]">
-                    <div className="flex flex-col justify-center text-center">
-                        {/* <p className="flex justify-center items-center font-lexend font-light text-base mb-4">
-                            <p className="mr-2"><RxDashboard /></p>
-                            Dashboard
-                        </p> */}
+                <div className="flex flex-col justify-between items-end min-h-[600px]">
+                    <div className="flex flex-col justify-center items-start w-[200px]">
                         {
                             navigation.map(link=>(
                                 <NavLink 
                                     to={link.to}
                                     className={({isActive}: NavLinkRenderProps)=>
                                     isActive ?
-                                    "!bg-[#7152F3]/5 flex justify-center items-center w-[220px] h-[50px] flex p-5 font-lexend font-semibold text-[#7152F3] text-base border-l-2 border-[#7152F3] mb-4" :
-                                    "w-[220px] h-[50px] flex justify-center items-center flex p-5 font-lexend font-light text-[#000] text-base mb-4"}>
-                                    {link.text}
+                                    "!bg-[#7152F3]/5 flex items-center w-full h-[50px] flex p-5 font-lexend font-semibold text-[#7152F3] text-base border-l-2 border-[#7152F3] mb-4" :
+                                    "border-2 border-yellow-500 w-full h-[50px] flex items-center p-5 font-lexend font-light text-[#000] text-base mb-4"}>
+                                    <p className="font-light text-base">{link.icon}</p>&nbsp;{link.text}
                                 </NavLink>
                             ))
                         }
-                        {/* <NavLink 
-                            to="/Faktura"
-                            className={({isActive}: NavLinkRenderProps)=>
-                            isActive ?
-                            "!bg-[#7152F3]/5 flex justify-center items-center w-[220px] h-[50px] flex p-5 font-lexend font-semibold text-[#7152F3] text-base border-l-2 border-[#7152F3] mb-4" :
-                            "w-[220px] h-[50px] flex justify-center items-center flex p-5 font-lexend font-light text-[#000] text-base mb-4"}>
-                            Fakturaer
-                        </NavLink>
-                        <NavLink 
-                            to="/Kunder"
-                            className={({isActive}: NavLinkRenderProps)=> 
-                            isActive ?
-                            "!bg-[#7152F3]/5 flex justify-center items-center w-[220px] h-[50px] flex p-5 font-lexend font-semibold text-[#7152F3] text-base border-l-2 border-[#7152F3] mb-4 " :
-                            "w-[220px] h-[50px] flex justify-center items-center flex p-5 font-lexend font-light text-[#000] text-base mb-4"}>
-                            Kunder
-                        </NavLink>
-                        <NavLink 
-                            to="/Profiler"
-                            className={({isActive}: NavLinkRenderProps)=> 
-                            isActive ?
-                            "!bg-[#7152F3]/5 flex justify-center items-center w-[220px] h-[50px] flex p-5 font-lexend font-semibold text-[#7152F3] text-base border-l-2 border-[#7152F3] mb-5" :
-                            "w-[220px] h-[50px] flex justify-center items-center flex p-5 font-lexend font-light text-[#000] text-base mb-5"}>
-                            Profiler
-                        </NavLink> */}
-                        {/* <p className="flex justify-center items-center font-lexend font-light text-base">
-                            <p className="mr-2"><IoSettingsOutline /></p>
-                            Settings
-                        </p> */}
                     </div>
                     <div className="flex justify-between">
                         <div className="flex justify-center items-center mr-2 w-[110px] h-[50px] bg-[#7152F3] rounded-md font-lexend font-light text-[#fff]">
