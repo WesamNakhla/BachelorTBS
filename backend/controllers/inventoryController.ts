330b3c5fe1fc4bdbc5468136a3215b21754ac101
atridox <siyabendgcc@hotmail.com>
2025-05-05 22:37:28 +0100

    :spakles: all the inventory api is working

--- backend/controllers/inventoryController.ts
+++ backend/controllers/inventoryController.ts
@@ -34,3 +34,106 @@ export const CreateInventory =  async(req: Request, res: Response, next: NextFun
           });
     }
 }
+export const DeleteInventory = async (req: Request, res: Response, next:NextFunction): Promise<void>=>{
+    const { id } = req.params;
+    try{
+        let customer = await Customer.findByIdAndDelete({ _id: id }).exec();
+        if(!customer){
+            res.status(404).json({
+                success: false,
+                message: "Inventory not found"
+            });
+            return 
+        }
+        res.status(200).json({
+            success: true,
+            message: "Inventory deleted successfully"
+        });
+        return 
+    }catch(error: any){
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+        return 
+    }
+}
+export const UpdateInventory = async (req: Request, res:Response, next:NextFunction): Promise<void>=>{
+    const {  arrival_date, sender, goods, quantity, weight,  departure_date } = req.body;
+    const { id } = req.params;
+    let updatedData = {
+        arrival_date: arrival_date,
+        sender: sender,
+        goods: goods,
+        quantity: quantity,
+        weight: weight,
+        departure_date: departure_date
+    }
+    try{
+        let updatedCustomer = await Customer.findByIdAndUpdate({ _id: id }, updatedData, { new: true }).exec();
+        if(!updatedCustomer){
+            res.status(404).json({
+                success: false,
+                message: "Customer not found"
+            });
+            return 
+        }
+        res.status(200).json({
+            success: true,
+            message: "Customer updated successfully"
+        });
+        return 
+    }catch(error: any){
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+        return 
+    }
+
+}
+export const getAllCustomerForInventory = async (req: Request, res: Response, next: NextFunction)=>{
+    try{
+        let customer = await Inventory.find({}).populate({ path: "customer", select: "customer -_id" }).exec();
+        if(!customer){
+            res.status(404).json({
+                success: true,
+                message: "No inventory found"
+            });
+            return ;
+        }
+        res.status(200).json({
+            success: true,
+            data: customer
+        });
+
+    }catch(error: any){
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+export const getAllInventoryPerCustomer = async (req: Request, res: Response, next: NextFunction): Promise<void>=>{
+    const { id } = req.params;
+    try{
+        let inventory = await Inventory.find({ customer: id }).select("arrival_date sender goods quantity weight  departure_date").lean();
+        if(!inventory){
+            res.status(404).json({
+                success: false,
+                message: "No inventory found"
+            });
+            return ;
+        }
+            res.status(404).json({
+                success: true,
+                data: inventory
+            });
+    }catch(error: any){
+
+            res.status(404).json({
+                success: false,
+                message: error.message
+            });
+    }
+}
--- backend/routes/inventoryRoutes.ts
+++ backend/routes/inventoryRoutes.ts
@@ -1,7 +1,17 @@
 import express from "express";
-import { CreateInventory } from "../controllers/inventoryController"
+import { 
+    CreateInventory,
+    UpdateInventory,
+    DeleteInventory,
+    getAllCustomerForInventory,
+    getAllInventoryPerCustomer
+ } from "../controllers/inventoryController"
 const router  = express.Router();
 
 router.post("/create-inventory", CreateInventory);
+router.put("/update-inventory/:id", UpdateInventory);
+router.delete("/delete-inventory/:id", DeleteInventory);
+router.get("/getAllCustomerInventory", getAllCustomerForInventory);
+router.get("/getAllInventoryPerCustomer/:id", getAllInventoryPerCustomer);
 
 export default router;
